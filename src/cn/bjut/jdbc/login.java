9b6f65302f903e329cc63b8bc9ebc2c1940bedf8
ggggyw <2326594384@qq.com>
2023-09-26 16:30:17 +0800

    Update login.java

--- src/cn/bjut/jdbc/login.java
+++ src/cn/bjut/jdbc/login.java
@@ -7,7 +7,7 @@ import javax.swing.*;
 public class login extends JFrame {
     private JTextField textField1;
     private JPasswordField passwordField;
-    private JComboBox<String> roleComboBox; // 添加角色选择下拉菜单
+    private JComboBox<String> roleComboBox;
 
     public login() {
         initComponents();
@@ -21,34 +21,34 @@ public class login extends JFrame {
         if (role != null) {
             if (role.equals("用户")) {
                 if (username.equals("user") && password.equals("userpassword")) {
-                      JOptionPane.showMessageDialog(this, "用户登录成功");
-//                    UserFrame userFrame = new UserFrame();
-//                    userFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//                    userFrame.setSize(400, 300);
-//                    userFrame.setVisible(true);
-//                    dispose();
+                    JOptionPane.showMessageDialog(this, "用户登录成功");
+                    UserFrm userFrame = new UserFrm();
+                    userFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                    userFrame.setSize(400, 300);
+                    userFrame.setVisible(true);
+                    dispose();
                 } else {
                     JOptionPane.showMessageDialog(this, "用户登录失败，请检查用户名和密码");
                 }
             } else if (role.equals("商家")) {
-               if (username.equals("merchant") && password.equals("merchantpassword")) {
-                     JOptionPane.showMessageDialog(this, "商家登录成功");
-//                    MerchantFrame merchantFrame = new MerchantFrame();
-//                    merchantFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//                    merchantFrame.setSize(400, 300);
-//                    merchantFrame.setVisible(true);
-//                    dispose();
+                if (username.equals("merchant") && password.equals("merchantpassword")) {
+                    JOptionPane.showMessageDialog(this, "商家登录成功");
+                    // MerchantFrame merchantFrame = new MerchantFrame();
+                    // merchantFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                    // merchantFrame.setSize(400, 300);
+                    // merchantFrame.setVisible(true);
+                    // dispose();
                 } else {
                     JOptionPane.showMessageDialog(this, "商家登录失败，请检查用户名和密码");
                 }
             } else if (role.equals("管理员")) {
                 if (username.equals("admin") && password.equals("adminpassword")) {
-                      JOptionPane.showMessageDialog(this, "管理员登录成功");
-//                    AdminFrame adminFrame = new AdminFrame();
-//                    adminFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//                    adminFrame.setSize(400, 300);
-//                    adminFrame.setVisible(true);
-//                    dispose();
+                    JOptionPane.showMessageDialog(this, "管理员登录成功");
+                    // AdminFrame adminFrame = new AdminFrame();
+                    // adminFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                    // adminFrame.setSize(400, 300);
+                    // adminFrame.setVisible(true);
+                    // dispose();
                 } else {
                     JOptionPane.showMessageDialog(this, "管理员登录失败，请检查用户名和密码");
                 }
@@ -63,25 +63,43 @@ public class login extends JFrame {
         JPanel contentPane = new JPanel();
         contentPane.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
         setContentPane(contentPane);
-        contentPane.setLayout(new GridLayout(4, 2, 5, 5)); // 增加一行用于放置角色选择
+        contentPane.setLayout(new GridBagLayout()); // 使用GridBagLayout布局管理器
+
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.insets = new Insets(5, 5, 5, 5); // 设置组件之间的间距
 
         JLabel label1 = new JLabel("Username:");
-        contentPane.add(label1);
+        constraints.gridx = 0; // 设置组件所在的列
+        constraints.gridy = 0; // 设置组件所在的行
+        contentPane.add(label1, constraints);
 
         textField1 = new JTextField();
-        contentPane.add(textField1);
+        constraints.gridx = 1;
+        constraints.gridy = 0;
+        constraints.fill = GridBagConstraints.HORIZONTAL; // 横向拉伸
+        contentPane.add(textField1, constraints);
 
         JLabel label2 = new JLabel("Password:");
-        contentPane.add(label2);
+        constraints.gridx = 0;
+        constraints.gridy = 1;
+        contentPane.add(label2, constraints);
 
         passwordField = new JPasswordField();
-        contentPane.add(passwordField);
+        constraints.gridx = 1;
+        constraints.gridy = 1;
+        constraints.fill = GridBagConstraints.HORIZONTAL;
+        contentPane.add(passwordField, constraints);
 
-        JLabel label3 = new JLabel("Role:"); // 添加角色选择标签
-        contentPane.add(label3);
+        JLabel label3 = new JLabel("Role:");
+        constraints.gridx = 0;
+        constraints.gridy = 2;
+        contentPane.add(label3, constraints);
 
-        roleComboBox = new JComboBox<>(new String[]{"用户", "商家", "管理员"}); // 添加角色选择下拉菜单
-        contentPane.add(roleComboBox);
+        roleComboBox = new JComboBox<>(new String[]{"用户", "商家", "管理员"});
+        constraints.gridx = 1;
+        constraints.gridy = 2;
+        constraints.fill = GridBagConstraints.HORIZONTAL;
+        contentPane.add(roleComboBox, constraints);
 
         JButton button1 = new JButton("Login");
         button1.addMouseListener(new MouseAdapter() {
@@ -90,7 +108,11 @@ public class login extends JFrame {
                 button1MouseClicked(e);
             }
         });
-        contentPane.add(button1);
+        constraints.gridx = 0;
+        constraints.gridy = 3;
+        constraints.gridwidth = 2; // 横向占据两列
+        constraints.anchor = GridBagConstraints.CENTER; // 居中对齐
+        contentPane.add(button1, constraints);
 
         pack();
     }
