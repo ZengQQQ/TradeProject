4ae7f86205ba865c041758d907de63964a297e05
778899dumingyang <3193946089@qq.com>
2023-09-26 10:57:55 +0800

    Create Forum.java

---
+++ src/cn/bjut/jdbc/Forum.java
@@ -0,0 +1,16 @@
+package cn.bjut.jdbc;
+
+import java.util.Date;
+
+public class Forum {
+
+    private int f_id;
+    private String f_con;
+    private Date f_time;
+
+    public Forum() {
+    }
+
+
+
+}
