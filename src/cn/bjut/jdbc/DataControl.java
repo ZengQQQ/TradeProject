5aa6f3474c374695b92a8d047c7bd65308da3d77
778899dumingyang <3193946089@qq.com>
2023-11-07 15:34:21 +0800

    Update DataControl.java

--- src/cn/bjut/jdbc/DataControl.java
+++ src/cn/bjut/jdbc/DataControl.java
@@ -920,7 +920,29 @@ public class DataControl {
 
             // 处理查询结果
             while (rs.next()) {
-                Order order = createOrderFromResultSet(rs);
+                Order order = new Order();
+                order.setP_id(rs.getInt("p_id"));
+                order.setU_id(rs.getInt("u_id"));
+                order.setBuytime(rs.getString("buy_time"));
+                order.setQuantity(rs.getInt("quantity"));
+                order.setTotalprice(String.valueOf(rs.getDouble("totalprice")));
+
+                Product product = new Product();
+                product.setP_name(rs.getString("p_name"));
+                product.setP_desc(rs.getString("p_desc"));
+                product.setP_class(rs.getString("p_class"));
+                product.setP_price(rs.getString("p_price"));
+                product.setP_quantity(rs.getInt("p_quantity"));
+                product.setP_img(rs.getString("p_img"));
+
+                User user = new User();
+                user.setU_name(rs.getString("u_name"));
+                user.setU_sex(rs.getString("u_sex"));
+                user.setU_tele(rs.getString("u_tele"));
+
+                order.setProduct(product);
+                order.setUser(user);
+
                 orderInfoList.add(order);
             }
         } catch (SQLException e) {
@@ -941,24 +963,12 @@ public class DataControl {
         Connection con = DataBase.OpenDB();
 
         try {
-            String sql = "SELECT o.p_id, o.u_id, o.buy_time, o.quantity, o.totalprice, p.p_name, p.p_desc, p.p_class, p.p_price, u.u_name, u.u_sex, u.u_tele FROM orders o ";
+            String sql = "SELECT o.p_id, o.u_id, o.buy_time, o.quantity, o.totalprice, p.p_name, p.p_desc, p.p_class, p.p_price,p_img,p_quantity, u.u_name, u.u_sex, u.u_tele FROM orders o ";
             String joinProduct = "INNER JOIN product p ON o.p_id = p.p_id ";
             String joinUser = "INNER JOIN user u ON o.u_id = u.u_id ";
             String whereClause = "WHERE o.m_id = ? ";
             List<Object> parameters = new ArrayList<>();
             parameters.add(m_id);
-
-            if (totalpricef != null && !totalpricef.isEmpty()) {
-                double totalPrice = Double.parseDouble(totalpricef);
-                if (Math.floor(totalPrice) == totalPrice) {
-                    whereClause += "AND ABS(o.totalprice - ?) < 1 ";
-                } else if (Math.round(totalPrice * 10.0) == totalPrice * 10.0) {
-                    whereClause += "AND CAST(o.totalprice AS DECIMAL(10, 1)" + ") = ? ";
-                } else {
-                    whereClause += "AND CAST(o.totalprice AS DECIMAL(10, 2)" + ") = ? ";
-                }
-                parameters.add(totalPrice);
-            }
 
             if (productf != null && !productf.isEmpty() && !productType.isEmpty()) {
                 switch (productType) {
@@ -984,7 +994,7 @@ public class DataControl {
                         }
                         break;
                     case "数量":
-                        whereClause += "AND o.quantity = ? ";
+                        whereClause += "AND p.p_quantity = ? ";
                         parameters.add(Integer.parseInt(productf));
                         break;
                     default:
@@ -1016,55 +1026,43 @@ public class DataControl {
                 parameters.add(Integer.parseInt(quantityf));
             }
 
+            if (totalpricef != null && !totalpricef.isEmpty()) {
+                double totalPrice = Double.parseDouble(totalpricef);
+                if (Math.floor(totalPrice) == totalPrice) {
+                    whereClause += "AND ABS(o.totalprice - ?) < 1 ";
+                } else if (Math.round(totalPrice * 10.0) == totalPrice * 10.0) {
+                    whereClause += "AND CAST(o.totalprice AS DECIMAL(10, 1)" + ") = ? ";
+                } else {
+                    whereClause += "AND CAST(o.totalprice AS DECIMAL(10, 2)" + ") = ? ";
+                }
+                parameters.add(totalPrice);
+            }
+
             if (datef != null && !datef.isEmpty()) {
                 switch (dateType) {
                     case "日期":
-                        // 检查输入是否为有效日期格式（例如 "2023.2.26"）
                         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy.M.d");
-                        try {
-                            Date inputDate = dateFormat.parse(datef);
-                            String startDate = new SimpleDateFormat("yyyy-MM-dd").format(inputDate);
-                            String endDate = new SimpleDateFormat("yyyy-MM-dd").format(inputDate) + " 23:59:59";
-
-                            whereClause += "AND o.buy_time >= ? AND o.buy_time <= ? ";
-                            parameters.add(startDate);
-                            parameters.add(endDate);
-                        } catch (ParseException e) {
-                            throw new RuntimeException(e);
-                        }
+                        Date inputDate = dateFormat.parse(datef);
+                        String startDate = new SimpleDateFormat("yyyy-MM-dd").format(inputDate);
+                        String endDate = new SimpleDateFormat("yyyy-MM-dd").format(inputDate) + " 23:59:59";
+                        whereClause += "AND o.buy_time >= ? AND o.buy_time <= ? ";
+                        parameters.add(startDate);
+                        parameters.add(endDate);
                         break;
                     case "年":
-                        // 检查输入是否为有效年份（例如 "2023"）
-                        try {
-                            int year = Integer.parseInt(datef);
-                            whereClause += "AND YEAR(o.buy_time) = ? ";
-                            parameters.add(year);
-                        } catch (NumberFormatException e) {
-                            // 处理无效的年份格式
-                            System.out.println("您输入的年份无效");
-                        }
+                        int year = Integer.parseInt(datef);
+                        whereClause += "AND YEAR(o.buy_time) = ? ";
+                        parameters.add(year);
                         break;
                     case "月":
-                        // 检查输入是否为有效月份（例如 "2"）
-                        try {
-                            int month = Integer.parseInt(datef);
-                            whereClause += "AND MONTH(o.buy_time) = ? ";
-                            parameters.add(month);
-                        } catch (NumberFormatException e) {
-                            // 处理无效的月份格式
-                            System.out.println("您输入的月份无效");
-                        }
+                        int month = Integer.parseInt(datef);
+                        whereClause += "AND MONTH(o.buy_time) = ? ";
+                        parameters.add(month);
                         break;
                     case "日":
-                        // 检查输入是否为有效日期（例如 "5"）
-                        try {
-                            int day = Integer.parseInt(datef);
-                            whereClause += "AND DAY(o.buy_time) = ? ";
-                            parameters.add(day);
-                        } catch (NumberFormatException e) {
-                            // 处理无效的日期格式
-                            System.out.println("您输入的日期无效");
-                        }
+                        int day = Integer.parseInt(datef);
+                        whereClause += "AND DAY(o.buy_time) = ? ";
+                        parameters.add(day);
                         break;
                     default:
                         break;
@@ -1087,6 +1085,8 @@ public class DataControl {
             stmt.close();
         } catch (SQLException e) {
             e.printStackTrace();
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
         } finally {
             try {
                 con.close();
@@ -1101,35 +1101,37 @@ public class DataControl {
         if (orderList.isEmpty() && (userf != null && !userf.isEmpty() || quantityf != null && !quantityf.isEmpty() || datef != null && !datef.isEmpty())) {
             JOptionPane.showMessageDialog(null, "订单结果没有找到", "搜索结果", JOptionPane.INFORMATION_MESSAGE);
             return null;
-        } else if (orderList.isEmpty() && ((totalpricef != null && !totalpricef.isEmpty()) || (productType.equals("价格") && (productf != null && !productf.isEmpty())))) {
+        } else if (orderList.isEmpty() && (totalpricef != null && !totalpricef.isEmpty())) {
             // 执行次要查询以根据指定的条件找到最接近的订单
-            JOptionPane.showMessageDialog(null, "订单结果没有找到，给您价格最接近的3个订单", "搜索结果", JOptionPane.INFORMATION_MESSAGE);
             return findClosestOrders(productf, totalpricef, m_id);
         }
 
         return orderList;
     }
 
+
     // 找到最接近的订单
-    private List<Order> findClosestOrders(String productPrice, String totalPrice, int m_id) throws SQLException {
+    private List<Order> findClosestOrders(String productPrice, String totalPrice, int m_id) {
         List<Order> closestOrders = new ArrayList<>();
         Connection con = DataBase.OpenDB();
 
         try {
             // 构建 SQL 查询基于可用的条件
-            String sql = "SELECT o.p_id, o.u_id, o.buy_time, o.quantity, o.totalprice, p.p_name, p.p_desc, p.p_class, p.p_price, u.u_name, u.u_sex, u.u_tele " +
+            String sql = "SELECT o.p_id, o.u_id, o.buy_time, o.quantity, o.totalprice, p.p_name, p.p_desc, p.p_class, p.p_price,p.p_img,p.p_quantity, u.u_name, u.u_sex, u.u_tele " +
                     "FROM orders o " +
                     "INNER JOIN product p ON o.p_id = p.p_id " +
                     "INNER JOIN user u ON o.u_id = u.u_id " +
                     "WHERE o.m_id = ? ";
 
             if (totalPrice != null && !totalPrice.isEmpty()) {
+                JOptionPane.showMessageDialog(null, "订单结果没有找到，给您订单总价格最接近的3个订单", "搜索结果", JOptionPane.INFORMATION_MESSAGE);
                 try {
                     sql += "ORDER BY ABS(o.totalprice - ?) LIMIT 3";
                 } catch (NumberFormatException e) {
                     throw new IllegalArgumentException("无效的总价格式。");
                 }
             } else if (productPrice != null && !productPrice.isEmpty()) {
+                JOptionPane.showMessageDialog(null, "订单结果没有找到，给您商品价格最接近的3个订单", "搜索结果", JOptionPane.INFORMATION_MESSAGE);
                 try {
                     sql += "ORDER BY ABS(p.p_price - ?) LIMIT 3";
                 } catch (NumberFormatException e) {
@@ -1177,6 +1179,8 @@ public class DataControl {
         product.setP_desc(rs.getString("p_desc"));
         product.setP_class(rs.getString("p_class"));
         product.setP_price(rs.getString("p_price"));
+        product.setP_img(rs.getString("p_img"));
+        product.setP_quantity(rs.getInt("p_quantity"));
 
         User user = new User();
         user.setU_name(rs.getString("u_name"));
