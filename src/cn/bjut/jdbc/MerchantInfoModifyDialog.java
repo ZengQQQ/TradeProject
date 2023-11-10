bf2362fe405c93fbb62e1105ce870de44ae2ec81
778899dumingyang <3193946089@qq.com>
2023-11-10 12:12:43 +0800

    Update MerchantInfoModifyDialog.java

--- src/cn/bjut/jdbc/MerchantInfoModifyDialog.java
+++ src/cn/bjut/jdbc/MerchantInfoModifyDialog.java
@@ -26,12 +26,14 @@ public class MerchantInfoModifyDialog extends JDialog {
         JTextField nameField = new JTextField(oldmerchant.getM_name(), 20);
         JTextField genderField = new JTextField(oldmerchant.getM_sex(), 20);
         JTextField phoneField = new JTextField(oldmerchant.getM_tele(), 20);
+        JTextField pswField = new JTextField(oldmerchant.getPsw(), 20);
 
         // 设置字体
         accountField.setFont(font);
         nameField.setFont(font);
         genderField.setFont(font);
         phoneField.setFont(font);
+        pswField.setFont(font);
 
         // 创建 "保存" 按钮
         JButton saveButton = new JButton("保存");
@@ -42,15 +44,24 @@ public class MerchantInfoModifyDialog extends JDialog {
             String newName = nameField.getText();
             String newGender = genderField.getText();
             String newPhone = phoneField.getText();
-            // 验证性别和电话号码等信息
-            if (!Pattern.matches("[\\u4e00-\\u9fa5a-zA-Z]+", newAccount)) {
-                JOptionPane.showMessageDialog(this, "账户只能为字母、中文和字符", "错误", JOptionPane.ERROR_MESSAGE);
+            String newPsw = pswField.getText();
+
+            // 验证账户、密码、姓名和电话号码
+            if (newAccount.isEmpty() || newName.isEmpty() || newGender.isEmpty() || newPhone.isEmpty() || newPsw.isEmpty()) {
+                JOptionPane.showMessageDialog(this, "所有字段都必须填写", "错误", JOptionPane.ERROR_MESSAGE);
                 return;
             }
-            if (!Pattern.matches("[\\u4e00-\\u9fa5a-zA-Z]+", newName)) {
-                JOptionPane.showMessageDialog(this, "姓名只能为字母、中文和字符", "错误", JOptionPane.ERROR_MESSAGE);
+
+            if (!Pattern.matches("[\\u4e00-\\u9fa5a-zA-Z]{1,20}", newAccount)) {
+                JOptionPane.showMessageDialog(this, "账户只能为字母、中文和字符，且长度不超过20", "错误", JOptionPane.ERROR_MESSAGE);
                 return;
             }
+
+            if (!Pattern.matches("[\\u4e00-\\u9fa5a-zA-Z]{1,20}", newName)) {
+                JOptionPane.showMessageDialog(this, "姓名只能为字母、中文和字符，且长度不超过20", "错误", JOptionPane.ERROR_MESSAGE);
+                return;
+            }
+
             if (!Pattern.matches("[男女]", newGender)) {
                 JOptionPane.showMessageDialog(this, "性别只能为男或女", "错误", JOptionPane.ERROR_MESSAGE);
                 return;
@@ -61,9 +72,15 @@ public class MerchantInfoModifyDialog extends JDialog {
                 return;
             }
 
+            if (!Pattern.matches("[\\u4e00-\\u9fa5a-zA-Z0-9]{1,15}", newPsw)) {
+                JOptionPane.showMessageDialog(this, "密码只能为字母、中文、字符和数字，且长度不超过15", "错误", JOptionPane.ERROR_MESSAGE);
+                return;
+            }
+
+
             // 保存修改后的信息并显示成功消息
             try {
-                data.updateMerchant(m_id, newAccount, newName, newGender, newPhone);
+                data.updateMerchant(m_id, newAccount, newName, newGender, newPhone,newPsw);
             } catch (SQLException ex) {
                 throw new RuntimeException(ex);
             }
@@ -76,6 +93,8 @@ public class MerchantInfoModifyDialog extends JDialog {
         JPanel panel = new JPanel(new GridLayout(6, 2));
         panel.add(new JLabel("账号名:")).setFont(new Font("微软雅黑", Font.BOLD, 16));
         panel.add(accountField).setFont(new Font("微软雅黑", Font.BOLD, 16));
+        panel.add(new JLabel("密码:")).setFont(new Font("微软雅黑", Font.BOLD, 16));
+        panel.add(pswField).setFont(new Font("微软雅黑", Font.BOLD, 16));
         panel.add(new JLabel("昵称:")).setFont(new Font("微软雅黑", Font.BOLD, 16));
         panel.add(nameField).setFont(new Font("微软雅黑", Font.BOLD, 16));
         panel.add(new JLabel("性别:")).setFont(new Font("微软雅黑", Font.BOLD, 16));
