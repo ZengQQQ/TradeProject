3c8fedf8af04ec59703c1ec6c9433826aa83fb1c
778899dumingyang <3193946089@qq.com>
2023-11-19 18:17:47 +0800

    Update DataControlMercahnt.java

--- src/cn/bjut/jdbc/DataControlMercahnt.java
+++ src/cn/bjut/jdbc/DataControlMercahnt.java
@@ -149,87 +149,75 @@ public class DataControlMercahnt extends DataControl {
     public List<Product> MerchantProductQueryByCategory(int m_id, String selectedCategory, String productId, String productName, String minPrice, String maxPrice, String minQuantity, String maxQuantity, String selectedStatus, String auditStatus) throws SQLException {
         List<Product> products = new ArrayList<>();
         Connection con = DataBase.OpenDB();
-        PreparedStatement stmt = null;
-        ResultSet rs = null;
+        PreparedStatement stmt;
+        ResultSet rs;
 
-        try {
-            StringBuilder sql = new StringBuilder("SELECT p_id, p_name, p_desc, p_class, p_price, p_status, p_quantity, p_img, p_auditStatus FROM product WHERE m_id = ?");
+        StringBuilder sql = new StringBuilder("SELECT p_id, p_name, p_desc, p_class, p_price, p_status, p_quantity, p_img, p_auditStatus FROM product WHERE m_id = ?");
 
-            List<Object> params = new ArrayList<>(Arrays.asList(m_id));
+        List<Object> params = new ArrayList<>(Arrays.asList(m_id));
 
-            if (!productId.isEmpty()) {
-                sql.append(" AND p_id = ?");
-                params.add(Integer.parseInt(productId));
-            }
-            if (!productName.isEmpty()) {
-                sql.append(" AND p_name LIKE ?");
-                params.add("%" + productName + "%");
-            }
-            if (!minPrice.isEmpty()) {
-                sql.append(" AND p_price >= ?");
-                params.add(Double.parseDouble(minPrice));
-            }
-            if (!maxPrice.isEmpty()) {
-                sql.append(" AND p_price <= ?");
-                params.add(Double.parseDouble(maxPrice));
-            }
-            if (!minQuantity.isEmpty()) {
-                sql.append(" AND p_quantity >= ?");
-                params.add(Integer.parseInt(minQuantity));
-            }
-            if (!maxQuantity.isEmpty()) {
-                sql.append(" AND p_quantity <= ?");
-                params.add(Integer.parseInt(maxQuantity));
-            }
-            if (!selectedStatus.isEmpty() && !"全部状态".equals(selectedStatus)) {
-                sql.append(" AND p_status = ?");
-                params.add(selectedStatus);
-            }
-            if (!auditStatus.isEmpty() && !"全部审核状态".equals(auditStatus)) {
-                sql.append(" AND p_auditStatus = ?");
-                params.add(auditStatus);
-            }
-            if (!selectedCategory.isEmpty() && !"全部类别".equals(selectedCategory)) {
-                sql.append(" AND p_class = ?");
-                params.add(selectedCategory);
-            }
-
-            stmt = con.prepareStatement(sql.toString());
-            for (int i = 0; i < params.size(); i++) {
-                stmt.setObject(i + 1, params.get(i));
-            }
-
-            rs = stmt.executeQuery();
-
-            while (rs.next()) {
-                Product product = new Product();
-                product.setP_id(rs.getInt("p_id"));
-                product.setP_name(rs.getString("p_name"));
-                product.setP_desc(rs.getString("p_desc"));
-                product.setP_class(rs.getString("p_class"));
-                product.setP_price(rs.getString("p_price"));
-                product.setP_status(rs.getString("p_status"));
-                product.setP_quantity(rs.getInt("p_quantity"));
-                product.setP_audiStatus(rs.getString("p_auditStatus"));
-                product.setP_img(rs.getString("p_img"));
-                products.add(product);
-            }
-        } finally {
-            // 关闭连接等资源（确保在使用完后关闭）
-            if (rs != null) {
-                rs.close();
-            }
-            if (stmt != null) {
-                stmt.close();
-            }
-            if (con != null) {
-                con.close();
-            }
+        if (!productId.isEmpty()) {
+            sql.append(" AND p_id = ?");
+            params.add(Integer.parseInt(productId));
         }
+        if (!productName.isEmpty()) {
+            sql.append(" AND p_name LIKE ?");
+            params.add("%" + productName + "%");
+        }
+        if (!minPrice.isEmpty()) {
+            sql.append(" AND p_price >= ?");
+            params.add(Double.parseDouble(minPrice));
+        }
+        if (!maxPrice.isEmpty()) {
+            sql.append(" AND p_price <= ?");
+            params.add(Double.parseDouble(maxPrice));
+        }
+        if (!minQuantity.isEmpty()) {
+            sql.append(" AND p_quantity >= ?");
+            params.add(Integer.parseInt(minQuantity));
+        }
+        if (!maxQuantity.isEmpty()) {
+            sql.append(" AND p_quantity <= ?");
+            params.add(Integer.parseInt(maxQuantity));
+        }
+        if (!selectedStatus.isEmpty() && !"全部状态".equals(selectedStatus)) {
+            sql.append(" AND p_status = ?");
+            params.add(selectedStatus);
+        }
+        if (!auditStatus.isEmpty() && !"全部审核状态".equals(auditStatus)) {
+            sql.append(" AND p_auditStatus = ?");
+            params.add(auditStatus);
+        }
+        if (!selectedCategory.isEmpty() && !"全部类别".equals(selectedCategory)) {
+            sql.append(" AND p_class = ?");
+            params.add(selectedCategory);
+        }
+        stmt = con.prepareStatement(sql.toString());
+        for (int i = 0; i < params.size(); i++) {
+            stmt.setObject(i + 1, params.get(i));
+        }
+        rs = stmt.executeQuery();
 
+        while (rs.next()) {
+            Product product = new Product();
+            product.setP_id(rs.getInt("p_id"));
+            product.setP_name(rs.getString("p_name"));
+            product.setP_desc(rs.getString("p_desc"));
+            product.setP_class(rs.getString("p_class"));
+            product.setP_price(rs.getString("p_price"));
+            product.setP_status(rs.getString("p_status"));
+            product.setP_quantity(rs.getInt("p_quantity"));
+            product.setP_audiStatus(rs.getString("p_auditStatus"));
+            product.setP_img(rs.getString("p_img"));
+            products.add(product);
+        }
+        // 关闭连接等资源（确保在使用完后关闭）
+        rs.close();
+        stmt.close();
+        con.close();
         return products;
     }
 
 
-
 }
+
